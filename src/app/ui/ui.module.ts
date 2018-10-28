39cc207a34d8db34ded3556f18777052e3de89d1
Ankit Mahule <ank.mahule@gmail.com>
2018-10-29 00:21:19 +0530

    Added cart component and font awesome.

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { UiRoutingModule } from './ui-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UiRoutingModule
   ],
-  declarations: [RegisterComponent, LoginComponent]
+  declarations: [RegisterComponent, LoginComponent, CartComponent]
 })
 export class UiModule { }
