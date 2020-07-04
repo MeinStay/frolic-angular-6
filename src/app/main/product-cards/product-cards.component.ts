92219ad16f01a080811451f05a594c28a656c425
Ankit Shailesh Mahule <ankit.mahule@contractors.roche.com>
2020-07-05 00:15:38 +0530

    1. Changes in registration form page. 2. Background of home page set. 3. New header color set.

--- src/app/main/product-cards/product-cards.component.ts
+++ src/app/main/product-cards/product-cards.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-cards',
-  templateUrl: './product-cards.component.html'
+  templateUrl: './product-cards.component.html',
+  styleUrls: ['./product-cards.component.scss']
 })
 export class ProductCardsComponent implements OnInit {
 
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UiRoutingModule } from './ui-routing.module';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { CartComponent } from './cart/cart.component';
+import { NgModule } from "@angular/core";
+import { CommonModule, FormStyle } from "@angular/common";
+import { UiRoutingModule } from "./ui-routing.module";
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import { CartComponent } from "./cart/cart.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    UiRoutingModule
-  ],
-  declarations: [RegisterComponent, LoginComponent, CartComponent]
+  imports: [CommonModule, UiRoutingModule, FormsModule],
+  declarations: [RegisterComponent, LoginComponent, CartComponent],
 })
-export class UiModule { }
+export class UiModule {}
